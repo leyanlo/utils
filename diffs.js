055b519fef2a42f516b36451e6f3921e79417ff2
Leyan Lo <leyan@uber.com>
2021-01-14 11:37:37 -0800

    feat(diffs): add script to log diffs with links

---
+++ diffs.js
@@ -0,0 +1,19 @@
+const { exec } = require("child_process");
+
+exec("git log origin/main..", (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const commits = (stdout + "\n")
+    .split(/commit [0-9a-f]{40}\n/)
+    .slice(1)
+    .reverse();
+  for (let commit of commits) {
+    const lines = commit.split("\n");
+    const title = lines[3].substr(4);
+    const link = lines[lines.length - 3].substr(27);
+    console.log(`- ${link} ${title}`);
+  }
+});
