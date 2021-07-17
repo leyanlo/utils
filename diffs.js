f13a0148b68fb244a441921db88f905601b3e67e
Leyan Lo <leyanlo@gmail.com>
2021-07-17 01:48:06 -0700

    chore(prettier): update prettier to use single quotes

--- diffs.js
+++ diffs.js
@@ -1,17 +1,17 @@
-const { exec } = require("child_process");
+const { exec } = require('child_process');
 
-exec(`git log ${process.argv[2] || "origin/main.."}`, (err, stdout, stderr) => {
+exec(`git log ${process.argv[2] || 'origin/main..'}`, (err, stdout, stderr) => {
   if (err) {
     console.error(err);
     return;
   }
 
-  const commits = (stdout + "\n")
+  const commits = (stdout + '\n')
     .split(/commit [0-9a-f]{40}\n/)
     .slice(1)
     .reverse();
   for (let commit of commits) {
-    const lines = commit.split("\n");
+    const lines = commit.split('\n');
     const title = lines[3].substr(4);
     const link = lines[lines.length - 3].substr(27);
     console.log(`- ${link} ${title}`);
